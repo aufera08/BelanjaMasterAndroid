872fc8d74648cc517278cd1c65571d8ff3d01b86
aufera <71977239+aufera@users.noreply.github.com>
2021-04-14 02:42:35 +0700

    First init

---
+++ app/src/main/java/com/aufera/belanja/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aufera.belanja
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
